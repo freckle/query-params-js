4084010cd2c4453cde8dfecbc07008d296c49834
AJ Ruf <z0isch@gmail.com>
2025-07-17 10:14:31 -0400

    Remove flow

--- dist/filters.js
+++ dist/filters.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.toQueryParamObj = void 0;
+exports.toQueryParamObj = toQueryParamObj;
 const map_1 = __importDefault(require("lodash/map"));
 const reduce_1 = __importDefault(require("lodash/reduce"));
 function toQueryParamObj(name, filters) {
@@ -11,4 +11,3 @@ function toQueryParamObj(name, filters) {
     const appended = (0, map_1.default)(entries, ([k, v]) => [`${name}[${k}]`, v]);
     return (0, reduce_1.default)(appended, (o, [k, v]) => (Object.assign(Object.assign({}, o), { [k]: v })), {});
 }
-exports.toQueryParamObj = toQueryParamObj;
